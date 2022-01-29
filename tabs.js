34d62b09853d5f88974b010334217ed42d0f6314
peninaseigel <peninaseigel@gmail.com>
2022-01-29 15:17:02 -0500

    updating css and adding tabs and temp js files

--- tabs.js
+++ tabs.js
@@ -1,1 +1,19 @@
-let;
+//
+
+function changeTab(event, tabName) {
+  // hide tab content divs
+  let tabContent = document.querySelectorAll(".tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  // remove "active" class from tablinks
+  let tabLinks = document.querySelectorAll(".tablinks");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  // Show current tab, and add "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
+}
