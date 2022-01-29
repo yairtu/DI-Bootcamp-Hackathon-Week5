761cb4fc794853ea7f76e5622873a931348093fd
yairtu <skiracer131@gmail.com>
2022-01-29 12:44:59 +0200

    currnecy id changes

--- Hackathon-Week5/script.js
+++ Hackathon-Week5/script.js
@@ -1,1 +1,18 @@
 //branch 1 is called first
+const currency1 = document.querySelector("#currecny1")
+
+
+let api_url = `https://v6.exchangerate-api.com/v6/596897f4ad88452b228ae09b/latest/USD`;
+
+
+const getConversion = () => {
+    const response = fetch(api_url)
+        .then((result) => result.json())
+        .then((data) => {
+            console.log(data);
+            let value = data.conversion_rates[`ILS`];
+            console.log(value);
+        })
+}
+
+getConversion();
