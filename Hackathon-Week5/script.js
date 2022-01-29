be3d904b66eda6d78975244f7544f6596d4b0937
yairtu <skiracer131@gmail.com>
2022-01-29 13:50:58 +0200

    working on conversion function

--- Hackathon-Week5/script.js
+++ Hackathon-Week5/script.js
@@ -1,18 +1,39 @@
 //branch 1 is called first
-const currency1 = document.querySelector("#currecny1")
+const currencyOne = document.querySelector("#currencyOne");
+const amountOne = document.querySelector("#currencyOneInput");
+const currencyTwo = document.querySelector("#currencyTwo");
+const amountTwo = document.querySelector("#currencyTwoInput");
+
+//Testing all elements respond
+console.log(currencyTwo.value)
+console.log(amountOne.value);
+console.log(currencyTwo.value);
+console.log(amountTwo.value)
+
+//Testing setting value of amountTwo;
+amountTwo.value = 5;
 
 
-let api_url = `https://v6.exchangerate-api.com/v6/596897f4ad88452b228ae09b/latest/USD`;
+
+// currencyOne.addEventListener("onchange", getConversionRate);
+// currencyOneInput.addEventListener("input", getConversionRate);
 
 
-const getConversion = () => {
-    const response = fetch(api_url)
-        .then((result) => result.json())
-        .then((data) => {
-            console.log(data);
-            let value = data.conversion_rates[`ILS`];
-            console.log(value);
-        })
-}
 
-getConversion();
+// const getConversionRate = () => {
+    //     const currecncyOneValue = currencyOne.value;
+    //     const currencyTwoValue = currencyTwo.value;
+    
+    //     let api_url = `https://v6.exchangerate-api.com/v6/596897f4ad88452b228ae09b/latest/${currecncyOneValue}`;
+        
+    //     const response = fetch(api_url)
+    //         .then((result) => result.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             rate = data.conversion_rates[`${currencyTwoValue}`];
+    //         })
+    //     console.log(rate)
+    //     currencyOneInputValue = currencyOneInput.value;
+    //     currencyTwoInputValue = currencyTwoInput.value;
+    //     currencyTwoInputValue = (currencyOneInputValue * rate);
+    // }
