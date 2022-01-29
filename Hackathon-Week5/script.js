49f6b48a5c5566c6f71371a0cc5296b0dca78c9b
yairtu <skiracer131@gmail.com>
2022-01-29 17:47:52 +0200

    working on conversion function

--- Hackathon-Week5/script.js
+++ Hackathon-Week5/script.js
@@ -1,39 +1,20 @@
-//branch 1 is called first
 const currencyOne = document.querySelector("#currencyOne");
-const amountOne = document.querySelector("#currencyOneInput");
 const currencyTwo = document.querySelector("#currencyTwo");
+const amountOne = document.querySelector("#currencyOneInput");
 const amountTwo = document.querySelector("#currencyTwoInput");
 
-//Testing all elements respond
-console.log(currencyTwo.value)
-console.log(amountOne.value);
-console.log(currencyTwo.value);
-console.log(amountTwo.value)
-
-//Testing setting value of amountTwo;
-amountTwo.value = 5;
+//testing if elemets are correct, works
+console.log(currencyOne);
+console.log(currencyTwo);
+console.log(amountOne);
+console.log(amountTwo);
 
 
 
-// currencyOne.addEventListener("onchange", getConversionRate);
-// currencyOneInput.addEventListener("input", getConversionRate);
+//event listener for dropdown and inputs
+currencyOne.addEventListener("onchange", calculateExchange);
+currencyTwo.addEventListener("onchange", calculateExchange);
+amountOne.addEventListener("oninput", calculateExchange);
+amountTwo.addEventListener("oninput", calculateExchange);
 
 
-
-// const getConversionRate = () => {
-    //     const currecncyOneValue = currencyOne.value;
-    //     const currencyTwoValue = currencyTwo.value;
-    
-    //     let api_url = `https://v6.exchangerate-api.com/v6/596897f4ad88452b228ae09b/latest/${currecncyOneValue}`;
-        
-    //     const response = fetch(api_url)
-    //         .then((result) => result.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             rate = data.conversion_rates[`${currencyTwoValue}`];
-    //         })
-    //     console.log(rate)
-    //     currencyOneInputValue = currencyOneInput.value;
-    //     currencyTwoInputValue = currencyTwoInput.value;
-    //     currencyTwoInputValue = (currencyOneInputValue * rate);
-    // }
