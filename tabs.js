e1538e35423fe651ca704f38f71da5243ac7fc52
peninaseigel <peninaseigel@gmail.com>
2022-01-29 11:22:10 -0500

    adding css file

---
+++ tabs.js
@@ -0,0 +1,21 @@
+let tabButton = document.querySelectorAll(".tablinks .tab .tab-links");
+
+let tabButtons2 = document.querySelectorAll(".wrapper");
+
+let tabcontainers = document.querySelectorAll(".tablinks .tab");
+
+// function tabs() {
+//   document.querySelectorAll(".tablinks").forEach((tabButton) => {
+//     tabButton.addEventListener("click", () => {
+//       let tabBar = tabButton.parentElement;
+//       let wrapper = tabBar.parentElement;
+//       let tabsNumber = tabButton.dataset.forTab;
+//       let tabToActivate = wrapper.querySelector(
+//         `.conversion-div[data-tab=${tabsNumber}`
+//       );
+//       tabBar.querySelectorAll(".tablinks").forEach(tabButton)
+
+//     });
+//   });
+// }
+// tabs();
